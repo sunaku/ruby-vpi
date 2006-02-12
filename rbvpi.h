2c21cb4e4b99e8c0315cc280e152b66094fe8ad1
Suraj N. Kurapati <sunaku@gmail.com>
2006-02-12 01:57:54 -0800

    add	GNUmakefile for easier compilation & testing add	Doxyfile for creating API docs
    refac	rbvpi.[ch] into sub-modules vlog.[ch]in & relay.[ch]in for easier maintenance
    note	these are named as *.[ch]in instead of *.[ch] because otherwise extconf.rb creates a makefile that tries to compile them. because we must use static functions to avoid namespace clashing when writing Ruby extensions, these files contain code fragments which should really all be in one big file. O_o

--- rbvpi.h
+++ rbvpi.h
@@ -18,61 +18,33 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA	 02110-1301	 USA
 */
+/**\file
+	VPI module, used by Ruby code.
+*/
 
 #ifndef RBVPI_H
 #define RBVPI_H
 
 	#include <ruby.h>
 
+	/**
+		Defines the VPI module, used by Ruby code.
+	*/
+	void Init_vpi();
 
-	/* Ensure that PLI_* storage types comply with IEEE Std 1364-2001 Version C (vpi_user.h), regardless of the Verilog simulator used. */
-	#ifndef PLI_TYPES
-		#define PLI_TYPES
-
-		#define PLI_INT32 int
-		#define PLI_UINT32 unsigned int
-		#define PLI_INT16 short
-		#define PLI_UINT16 unsigned short
-		#define PLI_BYTE8 char
-		#define PLI_UBYTE8 unsigned char
-	#endif
-
-	#include <vpi_user.h>
+	/**
+		Transfers control from Ruby code to Verilog code.
+	*/
+	static VALUE rbvpi_relay_verilog(VALUE rSelf);
 
 
+	/**
+		Registers a VPI task with the given name and associates it with the given Ruby block.
 
-	/* VPI module, used by Ruby code */
-		/**
-			Defines the VPI module, used by Ruby code.
-		*/
-		void Init_vpi();
+		For example, to register a VPI task named "$hello_world" to a Ruby block which prints the text "hello world", you can do:
+			<tt>VPI::register_systf("$hello_world") { puts "hello world" }</tt>
 
-		/**
-			Transfers control from Ruby code to Verilog code.
-		*/
-		static VALUE rbvpi_relay_verilog(VALUE self);
-
-
-
-
-	/* VPI tasks & callbacks, used by C and Verilog code */
-		/**
-			Transforms all arguments, which have been passed to the given task by Verilog, into a Ruby Array.
-		*/
-		static VALUE rbvpi_task_args(vpiHandle vTask);
-
-		/**
-			Binds a C function to a VPI task, so that Verilog code can invoke the C function via the VPI task.
-
-			For example, if we bound a VPI task named "$hello_world" to the C function ruby_world(), then the ruby_world() function would be invoked whenever Verilog code called the "$hello_world" VPI task.
-
-			@param	name	Name of the VPI task. For example, "$hello_world".
-
-			@param	func	The C function which you want to associate with the given VPI task. For example, ruby_world().
-		*/
-		static void rbvpi_bind_task(PLI_BYTE8* name, int (*func)(PLI_BYTE8*));
-
-		// TODO
-		// static void rbvpi_bind_callback();
+	*/
+	// static VALUE rbvpi_register_systf(VALUE rSelf, VALUE rTaskName, VALUE rTaskLogic);
 
 #endif
