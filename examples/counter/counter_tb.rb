647c9df5a5994a50fb941165f0a7e3535e9482f3
Suraj N. Kurapati <sunaku@gmail.com>
2006-05-14 16:49:58 -0700

    up	counter example to use RSpec

--- examples/counter/counter_tb.rb
+++ examples/counter/counter_tb.rb
@@ -4,70 +4,92 @@
 
 	This file is part of Ruby-VPI.
 
-	Ruby-VPI is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+	Ruby-VPI is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
-	Ruby-VPI is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+	Ruby-VPI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 =end
 
-# Note: DUT means "design under test"
-
 require 'vpi_util'
-require 'test/unit'
+require 'rspec'
+
+# An interface to the design under test.
+class Counter
+	BITS = 5
+	LIMIT = 2 ** BITS
+	MAX = LIMIT - 1
 
 
-class TestCounter < Test::Unit::TestCase
-	include Vpi
+	attr_reader :clock, :reset, :count
 
-	# Number of bits used in the counter.
-	COUNTER_BITS = 5
-	COUNTER_LIMIT = 2 ** COUNTER_BITS
+	def initialize
+		@clock = Vpi::vpi_handle_by_name("counter_tb.clock", nil)
+		@reset = Vpi::vpi_handle_by_name("counter_tb.reset", nil)
+		@count = Vpi::vpi_handle_by_name("counter_tb.count", nil)
+	end
+end
 
-	# Number of cycles needed to reset the DUT
-	DUT_RESET_DELAY = 5
+# verify the design
+include Vpi
 
+	# resets the given design
+	def reset aDesign
+		aDesign.reset.intVal = 1
+		3.times {relay_verilog}
+		aDesign.reset.intVal = 0
 
-	def setup
-		@dut = vpi_handle_by_name("counter_tb", nil)
-		@dut_reset = vpi_handle_by_name("counter_tb.reset", nil)
-		@dut_count = vpi_handle_by_name("counter_tb.count", nil)
-
-		reset
+		@design.count.intVal.should.be 0
 	end
 
-	# Resets the DUT.
-	def reset
-		@dut_reset.intVal = 1
-		DUT_RESET_DELAY.times {relay_verilog}
+context "A resetted Counter" do
+	setup do
+		@design = Counter.new
 
-		@dut_reset.intVal = 0
+		# reset the counter
+		reset @design
 	end
 
-	def test_count
-		COUNTER_LIMIT.times do |i|
-			assert_equal i, @dut_count.intVal
+	specify "should be zero" do
+		@design.count.intVal.should.be 0
+		relay_verilog
+	end
 
-			# increment the counter
+	specify "should increment every cycle" do
+		Counter::LIMIT.times do |i|
+			@design.count.intVal.should.be i
 			relay_verilog
 		end
+	end
+end
 
-		assert_equal 0, @dut_count.intVal, "counter should overflow"
+context "A counter with the maximum value" do
+	setup do
+		@design = Counter.new
+
+		# reset the counter
+		reset @design
+
+		# increment to maximum value
+		0.upto(Counter::MAX) {relay_verilog}
+		@design.count.intVal.should.be Counter::MAX
+	end
+
+	specify "should overflow upon increment" do
+		# increment the counter
+		relay_verilog
+
+		@design.count.intVal.should.be 0
+		relay_verilog
 	end
 end
 
 
-# $ruby_init():
-Vpi::relay_verilog
+# bootstrap this file
+if $0 == __FILE__
+	# service the $ruby_init() callback
+	Vpi::relay_verilog
 
-
-# $ruby_relay():
-# do nothing here, because test/unit will automatically run the unit test above
+	# service the $ruby_relay() callback
+	# RSpec will take control from here.
+end
--- examples/counter/counter_tb.v
+++ examples/counter/counter_tb.v
@@ -4,55 +4,43 @@
 
 	This file is part of Ruby-VPI.
 
-	Ruby-VPI is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+	Ruby-VPI is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
-	Ruby-VPI is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+	Ruby-VPI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Note: DUT means "design under test"
-
-// The whole Ruby-VPI code is bootstrapped from this file. So, the intent is that a verilog testbench invokes the Ruby VPI extension, which in turn performs some extra things for us.
 module counter_tb;
 
-	reg clk;
-	reg reset;
-	wire [4:0] count;
+	/* configuration for the DUT */
+	parameter Size = 5;
 
 
+	/* accessors for the DUT */
+	reg clock	;
+reg reset	;
+wire  [Size - 1:0] count;
+
+
+	/* instantiate the DUT */
+	counter #(.Size(Size)) counter_tb_dut (.clock(clock), .reset(reset), .count(count));
+
+
+	/* interface to Ruby-VPI */
 	initial begin
-		#0 $ruby_init("-w", "counter_tb.rb");
-		#1 clk = 0; reset = 0;
+		clock = 0;
+		$ruby_init("-w", "counter_tb.rb", "-f", "s");
 	end
 
-
-	// generate a 50% duty-cycle clock for the DUT
+	/* generate a 50% duty-cycle clock for the DUT */
 	always begin
-		#5 clk = ~clk;
+		#5 clock = ~clock;
 	end
 
-
-	// transfer control to Ruby-VPI every clock cycle
-	always @(posedge clk) begin
+	/* transfer control to Ruby-VPI every clock cycle */
+	always @(posedge clock) begin
 		$ruby_relay();
 	end
-
-
-	// instantiate the DUT
-	counter dut(
-		.clock(clk)
-		, .reset(reset)
-
-		, .count(count)
-	);
 
 endmodule
