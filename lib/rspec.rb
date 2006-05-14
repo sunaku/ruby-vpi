77c6fcc45e5e591e1d8ee4721bb0a71123bdafcf
Suraj N. Kurapati <sunaku@gmail.com>
2006-05-14 00:05:46 -0700

    fix	compatibility for rspec 0.5.4

--- lib/rspec.rb
+++ lib/rspec.rb
@@ -1,12 +1,11 @@
 # Bootstraps the RSpec library from within Ruby.
 
 require 'rubygems'
-require_gem 'rspec'
+require_gem 'rspec', '>= 0.5.4'
 require 'spec'
 
 # prevent RSpec termination when no arguments are provided
 ARGV.unshift ''
 
-context_runner = Spec::Runner::ContextRunner.new(ARGV)
-Spec::Runner::Context.context_runner = context_runner
-at_exit {context_runner.run}
+$context_runner = ::Spec::Runner::OptionParser.create_context_runner(ARGV, false, STDERR, STDOUT)
+at_exit {$context_runner.run true}
