276a90db2a76f3382f86c02fbf988f1f7930392e
Suraj N. Kurapati <sunaku@gmail.com>
2006-05-20 22:53:19 -0700

    fix	'stand alone' mode was executing contexts twice

--- lib/rspec.rb
+++ lib/rspec.rb
@@ -4,5 +4,9 @@ require 'rubygems'
 require_gem 'rspec', '>= 0.5.4'
 require 'spec'
 
-$context_runner = ::Spec::Runner::OptionParser.create_context_runner(ARGV, true, STDERR, STDOUT)
+# prevent RSpec termination when no arguments are provided
+ARGV.unshift ''
+
+
+$context_runner = ::Spec::Runner::OptionParser.create_context_runner(ARGV, false, STDERR, STDOUT)
 at_exit {$context_runner.run false}
