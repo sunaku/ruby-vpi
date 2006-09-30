8b1220d59f8d3f3dea9ed13288484bda0435db6b
Suraj N. Kurapati <sunaku@gmail.com>
2006-09-30 08:58:56 -0700

    incorporate output of test generator

--- samp/counter/counter_rspecTest_bench.v
+++ samp/counter/counter_rspecTest_bench.v
@@ -5,11 +5,11 @@ module counter_rspecTest_bench;
   // instantiate the design under test
     parameter Size = 5;
 
-    reg clock;
-    reg reset;
-    wire  [Size - 1 : 0] count;
+    reg  clock;
+    reg  reset;
+    wire [Size - 1 : 0] count;
 
-    counter#(.Size(Size))counter_rspecTest_bench_rspecTest_design(.clock(clock), .reset(reset), .count(count));
+    counter #(.Size(Size))counter_rspecTest_bench_design(.clock(clock), .reset(reset), .count(count));
 
   // connect to the Ruby side of this bench
     initial begin
--- samp/counter/counter_unitTest_bench.v
+++ samp/counter/counter_unitTest_bench.v
@@ -1,15 +1,17 @@
 /* This is the Verilog side of the bench. */
 
+
+
 module counter_unitTest_bench;
 
   // instantiate the design under test
     parameter Size = 5;
 
-    reg clock;
-    reg reset;
-    wire  [Size - 1 : 0] count;
+    reg  clock;
+    reg  reset;
+    wire [Size - 1 : 0] count;
 
-    counter#(.Size(Size))counter_unitTest_bench_unitTest_design(.clock(clock), .reset(reset), .count(count));
+    counter #(.Size(Size))counter_unitTest_bench_design(.clock(clock), .reset(reset), .count(count));
 
   // connect to the Ruby side of this bench
     initial begin
