b65c08b97591c2ef6a904fb0ec22179ec45f0c96
Suraj N. Kurapati <sunaku@gmail.com>
2006-11-05 15:16:57 -0800

    used the less confusing singleton method notation

--- bin/generate_test_tpl/proto.rb
+++ bin/generate_test_tpl/proto.rb
@@ -1,15 +1,14 @@
 # This is a prototype of the design under test.
-class << <%= aOutputInfo.designClassName %>
-  # When prototyping is enabled, this method is invoked
-  # instead of relay_verilog to simulate the design.
-  def simulate!
-    # discard old outputs
+
+# When prototyping is enabled, relay_verilog invokes this method
+# instead of transferring control to the Verilog simulator.
+def <%= aOutputInfo.designClassName %>.simulate!
+  # discard old outputs
 <% aModuleInfo.ports.reject { |p| p.input? }.each do |port| %>
-      <%= port.name %>.hexStrVal = 'x'
+    <%= port.name %>.hexStrVal = 'x'
 <% end %>
 
-    # process new inputs
+  # process new inputs
 
-    # produce new outputs
-  end
+  # produce new outputs
 end
