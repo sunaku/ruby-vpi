322cadebf7f05dcd3fc1833a4f0850d3dc35761e
Suraj N. Kurapati <sunaku@gmail.com>
2006-12-03 17:00:33 -0800

    fix content for note about "fixed in 2.0.0" was not appearing in manual

--- doc/manual.rb
+++
@@ -1,5 +0,0 @@
-# This is a helper file that gets loaded before manual.erb is evaluated.
-
-@fixed_in_2_0_0 = note("Fixed in 2.0.0.") do
-  "This problem was fixed in release 2.0.0 (2006-04-17)."
-end
