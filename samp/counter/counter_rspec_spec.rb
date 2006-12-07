1d820480543dcfe043ce4874f79507fca624cf25
Suraj N. Kurapati <sunaku@gmail.com>
2006-12-07 13:37:39 -0800

    change should_equal to "should

--- samp/counter/counter_rspec_spec.rb
+++ samp/counter/counter_rspec_spec.rb
@@ -12,12 +12,12 @@ context "A resetted counter's value" do
   end
 
   specify "should be zero" do
-    Counter.count.intVal.should_equal 0
+    Counter.count.intVal.should == 0
   end
 
   specify "should increment by one count upon each rising clock edge" do
     LIMIT.times do |i|
-      Counter.count.intVal.should_equal i
+      Counter.count.intVal.should == i
       relay_verilog # increment the counter
     end
   end
@@ -29,11 +29,11 @@ context "A counter with the maximum value" do
 
     # increment the counter to maximum value
     MAX.times {relay_verilog}
-    Counter.count.intVal.should_equal MAX
+    Counter.count.intVal.should == MAX
   end
 
   specify "should overflow upon increment" do
     relay_verilog # increment the counter
-    Counter.count.intVal.should_equal 0
+    Counter.count.intVal.should == 0
   end
 end
