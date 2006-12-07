1178e4f5331c6f615d09b2909d5a27d2ae5b0d0d
Suraj N. Kurapati <sunaku@gmail.com>
2006-12-07 13:25:23 -0800

    fix reset signal was not asserted long enough, so the DUT was behaving non-deterministically

--- samp/counter/counter_rspec_design.rb
+++ samp/counter/counter_rspec_design.rb
@@ -2,7 +2,8 @@
 
 # This method resets the design under test.
 def Counter.reset!
+  # assert the reset signal for five clock cycles
   reset.intVal = 1
-  relay_verilog
+  5.times {relay_verilog}
   reset.intVal = 0
 end
