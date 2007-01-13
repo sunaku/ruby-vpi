1e211a7f7d5ca100b8235527e0c62e446340c0da
Suraj N. Kurapati <sunaku@gmail.com>
2007-01-13 15:39:20 -0800

    remove checking of pthread & ruby libs because extconf takes care of it

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,2 @@
 require 'mkmf'
-require 'rbconfig'
-
-have_library('pthread', 'pthread_create') &&
-have_library(Config::CONFIG['RUBY_SO_NAME'] || 'ruby', 'ruby_init') &&
-
 create_makefile('ruby-vpi')
