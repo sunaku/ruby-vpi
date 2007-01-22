d08a6c5c5d25a17bfc8677dfa520cfed4edb4d43
Suraj N. Kurapati <sunaku@gmail.com>
2007-01-22 15:26:08 -0800

    add checks for pthreads & ruby libraries before creating makefile
    - otherwise .so file was not getting linked properly on MASC machine

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,2 +1,19 @@
 require 'mkmf'
-create_makefile('ruby-vpi')
+
+# check for POSIX threads library
+  hasPthread = have_library('pthread', 'pthread_create')
+
+# check for ruby library
+  require 'rbconfig'
+
+  rubyLibArgs = Config::CONFIG.values.grep(/^-lruby/)
+
+  rubyLibNames = rubyLibArgs.map {|a| a.sub /^-l/, ''}
+  rubyLibNames.unshift 'ruby' # try most common name first
+  rubyLibNames.uniq!
+
+  hasRuby = rubyLibNames.inject(false) do |verdict, name|
+    verdict ||= have_library(name, 'ruby_init')
+  end
+
+hasPthread && hasRuby && create_makefile('ruby-vpi')
