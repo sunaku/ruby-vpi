dd80fc68dd14f1229bbb200d2a291623a978babc
Suraj N. Kurapati <sunaku@gmail.com>
2007-04-28 23:33:08 -0700

    sync with Rassmalog's config/format.rb

--- doc/lib/doc_format.rb
+++ doc/lib/doc_format.rb
@@ -24,29 +24,49 @@ require 'coderay'
 require 'redcloth'
 
 class String
-  # The content of these HTML tags will be preserved verbatim when they are processed by Textile.
+  # The content of these HTML tags will be preserved verbatim when they are
+  # processed by Textile.
   PRESERVED_TAGS = [:code, :tt]
 
   # Transforms this string into HTML.
   def to_html
     text = dup
 
-    # prevent the content of these tags from being transformed by Textile
-    # for example, Textile transforms quotation marks in code into curly ones (&#8192;) -- this ruins any source code in the content of the tags!
+    # prevent the content of these tags from being transformed by Textile.
+    # for example, Textile transforms quotation marks in code into curly ones
+    # (&#8192;) -- this ruins any source code in the content of the tags!
       PRESERVED_TAGS.each do |tag|
-        text.gsub! %r{<#{tag}(.*?)>(.*?)</#{tag}>}m, %{<pre tag=#{tag.inspect}\\1>\\2</pre>}
+        text.gsub! \
+          %r{<#{tag}(.*?)>(.*?)</#{tag}>}m,
+          %{<pre tag=#{tag.inspect}\\1>\\2</pre>}
       end
 
     html = text.redcloth
 
+    # redcloth wraps a single item within paragraph tags, which prevents the
+    # item's HTML from being validly injected within other block-level
+    # elements, such as headings (h1, h2, etc.)
+    html.sub! %r{^<p>(.*)</p>$}m do |match|
+      payload = $1
+
+      if payload =~ /<p>/
+        match
+      else
+        payload
+      end
+    end
+
     # restore the original tags for the preserved tags
-      # unescape content of <pre> tags because they may contain nested preserved tags (redcloth escapes the content of <pre> tags)
+      # unescape content of <pre> tags because they may contain nested
+      # preserved tags (redcloth escapes the content of <pre> tags)
         html.gsub! %r{(<pre>)(.*?)(</pre>)}m do
           $1 + CGI.unescapeHTML($2) + $3
         end
 
       PRESERVED_TAGS.each do |tag|
-        html.gsub! %r{<pre tag=#{tag.inspect}(.*?)>(.*?)</pre>}m, %{<#{tag}\\1>\\2</#{tag}>}
+        html.gsub! \
+          %r{<pre tag=#{tag.inspect}(.*?)>(.*?)</pre>}m,
+          %{<#{tag}\\1>\\2</#{tag}>}
       end
 
       # assume that indented text in Textile is NOT source code
@@ -65,10 +85,10 @@ class String
     RedCloth.new(self).to_html
   end
 
-  # Adds syntax coloring to <code> elements in the given text. If
-  # the <code> tag has an attribute lang="...", then that is considered the
-  # programming language for which appropriate syntax coloring should be
-  # applied. Otherwise, the programming language is assumed to be ruby.
+  # Adds syntax coloring to <code> elements in the given text. If the <code>
+  # tag has an attribute lang="...", then that is considered the programming
+  # language for which appropriate syntax coloring should be applied.
+  # Otherwise, the programming language is assumed to be ruby.
   def coderay
     gsub %r{<(code)(.*?)>(.*?)</\1>}m do
       code = CGI.unescapeHTML $3
