d5658cd855dcebaee8be161c45329c749e77dfca
Suraj N. Kurapati <sunaku@gmail.com>
2007-07-10 21:37:03 -0700

    remove clock-signal diagrams & translation of `define macros into Ruby in design.rb

--- bin/generate/design.rb
+++ bin/generate/design.rb
@@ -1,92 +1,16 @@
-<% aParseInfo.constants.each do |var| %>
-<%= var.name.to_ruby_const_name %> = <%= var.value.verilog_to_ruby %>
-<% end %>
-
-# Brings the design under test into a blank state.
-def reset!
-<% aModuleInfo.input_ports.each do |port| %>
-  <%= port.name %>.x!
-<% end %>
-end
-
-<% clock = aModuleInfo.clock_port.name %>
+<% clock = aModuleInfo.clock_port.name rescue "YOUR_CLOCK_SIGNAL_HERE" %>
 # Simulates the design under test for one clock cycle.
 def cycle!
-  ##
-  # We are currently at the position indicated by the exclamation marks (!):
-  #
-  #          !
-  #          !
-  #          !    ____________________                        _________________
-  #          !   /                    \                      /
-  #          !  /                      \                    /
-  # _________!_/                        \__________________/
-  #          !
-  #          !
-  #
-  ##
-
   <%= clock %>.high!
-
-  ##
-  # After setting the clock signal to high, we are here:
-  #
-  #                !
-  #                !
-  #               _!__________________                        _________________
-  #              / !                  \                      /
-  #             /  !                   \                    /
-  # ___________/   !                    \__________________/
-  #                !
-  #                !
-  #
-  ##
-
   advance_time
-
-  ##
-  # After advancing the time, we are here:
-  #
-  #                                 !
-  #                                 !
-  #               __________________!_                        _________________
-  #              /                  ! \                      /
-  #             /                   !  \                    /
-  # ___________/                    !   \__________________/
-  #                                 !
-  #                                 !
-  #
-  ##
-
   <%= clock %>.low!
-
-  ##
-  # After setting the clock signal to low, we are here:
-  #
-  #                                       !
-  #                                       !
-  #               ____________________    !                   _________________
-  #              /                    \   !                  /
-  #             /                      \  !                 /
-  # ___________/                        \_!________________/
-  #                                       !
-  #                                       !
-  #
-  ##
-
   advance_time
+end
 
-  ##
-  # After advancing the time, we are here:
-  #
-  #                                                      !
-  #                                                      !
-  #               ____________________                   !    _________________
-  #              /                    \                  !   /
-  #             /                      \                 !  /
-  # ___________/                        \________________!_/
-  #                                                      !
-  #                                                      !
-  #
-  ##
+<% reset = aModuleInfo.reset_port.name rescue "YOUR_RESET_SIGNAL_HERE" %>
+# Brings the design under test into a blank state.
+def reset!
+  <%= reset %>.high!
+  cycle!
+  <%= reset %>.low!
 end
--- bin/generate/proto.rb
+++ bin/generate/proto.rb
@@ -1,6 +1,6 @@
 # Ruby prototype of the design under test's Verilog implementation.
 def feign!
-  if <%= aModuleInfo.clock_port.name %>.posedge?
+  if <%= aModuleInfo.clock_port.name rescue "YOUR_CLOCK_SIGNAL_HERE" %>.posedge?
     # discard old outputs
     <% aModuleInfo.output_ports.each do |port| %>
       <%= port.name %>.x!
