f0b1b2cce9cd552ff4925b53bc4bd935794d52b7
Suraj N. Kurapati <sunaku@gmail.com>
2007-08-01 09:20:42 -0700

    add rdoc comments

--- lib/ruby-vpi/util.rb
+++ lib/ruby-vpi/util.rb
@@ -1,10 +1,16 @@
+# A small utility library.
+#--
+# Copyright 2006 Suraj N. Kurapati
+# See the file named LICENSE for details.
+
 class String
   # Converts this string into a valid Ruby constant name.
   def to_ruby_const_name
     self[0, 1].upcase << self[1..-1]
   end
 
-  def rstrip_from a
-    sub(/#{a}[^#{a}]*$/, '')
+  # Strips off everything after the given character in this string.
+  def rstrip_from char
+    sub(/#{char}[^#{char}]*$/, '')
   end
 end
