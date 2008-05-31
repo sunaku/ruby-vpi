121be2a98dce0c228e0d25fde402e4624d5abd9d
Suraj N. Kurapati <sunaku@gmail.com>
2008-05-31 13:48:37 -0700

    remove pipelined ALU example because it is buggy

--- examples/pipelined_alu/hw5_unit_loader.rb
+++
@@ -1,1 +0,0 @@
-RubyVPI.load_test("hw5_unit", "hw5_unit_design.rb", "hw5_unit_proto.rb", "hw5_unit_spec.rb")
--- examples/pipelined_alu/hw5_unit_proto.rb
+++
@@ -1,4 +0,0 @@
-if RubyVPI::USE_PROTOTYPE
-  warn "Woe is me! Nobody implemented a Ruby prototype for this design. :-("
-  exit
-end
