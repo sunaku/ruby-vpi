76ad48dd97ffebc081dd4577a3caff5fa2bd02f5
Suraj N. Kurapati <sunaku@gmail.com>
2009-09-20 15:27:20 -0700

    upgrade to Ruby 1.9.1p243

--- ext/host.c
+++ ext/host.c
@@ -10,43 +10,30 @@
 #include <ruby.h>
 #include <stdlib.h>
 
-VALUE RubyVPI_host_gProgName;
-
-#ifdef RUBY_GLOBAL_SETUP
-    RUBY_GLOBAL_SETUP
-#endif
-
 PLI_INT32 RubyVPI_host_init(p_cb_data aCallback)
 {
     //
     // ruby init
     //
 
-    #ifdef RUBY_INIT_STACK
-        RubyVPI_util_debug("Host: RUBY_INIT_STACK");
-        RUBY_INIT_STACK;
-    #endif
+    int argc = 0;
+    char** argv = {""};
+    RubyVPI_util_debug("Host: ruby_sysinit(%d, %p)", argc, argv);
+    ruby_sysinit(&argc, &argv);
+
+    VALUE dummy;
+    VALUE* stack_start = &dummy + 0x1000;
+    RubyVPI_util_debug("Host: ruby_init_stack(%p)", stack_start);
+    ruby_init_stack(stack_start);
 
     RubyVPI_util_debug("Host: ruby_init()");
     ruby_init();
 
-    // override Ruby's hooked handlers for $0 so that $0 can be
-    // treated as pure Ruby value (and modified without restriction)
-    RubyVPI_util_debug("Host: redefine $0 hooked variable");
-    RubyVPI_host_gProgName = rb_str_new2("ruby-vpi");
-    rb_define_variable("$0", &RubyVPI_host_gProgName);
-    rb_define_variable("$PROGRAM_NAME", &RubyVPI_host_gProgName);
-
     RubyVPI_util_debug("Host: ruby_init_loadpath()");
     ruby_init_loadpath();
 
-    #ifdef HAVE_RUBY_1_9
-        RubyVPI_util_debug("Host: ruby_init_gems(Qtrue)");
-        rb_const_set(rb_define_module("Gem"), rb_intern("Enable"), Qtrue);
-
-        RubyVPI_util_debug("Host: Init_prelude()");
-        Init_prelude();
-    #endif
+    RubyVPI_util_debug("Host: ruby_script()");
+    ruby_script("ruby-vpi");
 
 
     //
@@ -72,8 +59,8 @@ PLI_INT32 RubyVPI_host_fini(p_cb_data aCallback)
     RubyVPI_util_debug("Host: user fini");
     RubyVPI_user_fini();
 
-    RubyVPI_util_debug("Host: ruby_finalize()");
-    ruby_finalize();
+    RubyVPI_util_debug("Host: ruby_cleanup()");
+    ruby_cleanup(0);
 
     return 0;
 }
